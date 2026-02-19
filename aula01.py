306b1e0b856a1cda5ca0875cf93e30624065ed68
Maria Vítória <mariavitoriagoncalves1983@gmail.com>
2026-02-19 10:04:31 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variável inteiro
+peso = 75.7 # variável float 
+nome = "Maria Vitória Gonçalves" # variável string
+sexo_feminino = True # variável bool 
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)

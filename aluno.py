88c070d35c80ee8d41345778f4f089f677eb38dc
maria vitoria <mariavitoriagoncalves1983@gmail.com>
2026-02-20 09:50:12 -0300

    aluno

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome = input("digite o teu nome: ")
+idade = int(input("tua idade é: "))
+telefone = int(input("teu telefone é: "))
+cidade = input("digite tua cidade: ")
+
+print("ola", nome)
+print("tua idade é", idade)
+print("teu telefone é", telefone)
+print("tua cidade é", cidade)
---
+++ matematica.py
@@ -0,0 +1,12 @@
+valor1 = int(input("digite o primeiro valor"))
+valor2 = int(input("digite o segundo valor"))
+
+soma = valor1+valor2
+subtração = valor1-valor2
+divisão = valor1/valor2
+multiplicação = valor1 * valor2
+
+print("a soma é" , soma)
+print("a subtração é", subtração)
+print("a divisão é", divisão)
+print("a multiplicação é", multiplicação)

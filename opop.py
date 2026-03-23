ba775496a90b52e63f34db92b02ec4d3faa1b622
maria <mariavitoriagoncalves1983@gmail.com>
2026-03-23 12:46:20 -0300

    23/03

---
+++ opop.py
@@ -0,0 +1,5 @@
+acoes = ["Digitar texto", "Mudar cor", "Inserir imagem"]
+print(f"lista atual: {acoes}")
+acoes.pop()
+
+print(f"lista atualizada: {acoes}")
---
+++ sistema.py
@@ -0,0 +1,10 @@
+autorizados = ["Alice", "Bob", "Carlos"]
+nome = input("Digite o nome do pesquisador")
+
+if nome in autorizados:
+    print(f"Acesso Permitido! O pesquisador {nome} está na posição {indice}")
+
+    remover = input("Deseja remover esse pesquisador da lista? S/N: ")
+    if remover == "S"
+
+

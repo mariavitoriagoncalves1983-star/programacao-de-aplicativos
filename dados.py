3ae98ed9fb02ab7aa59e19f766f0eafa0377dd01
maria <mariavitoriagoncalves1983@gmail.com>
2026-03-26 09:57:13 -0300

    26/03

---
+++ dados.py
@@ -0,0 +1,13 @@
+compras = []
+compra = input("Digite o nome do produto (ou 'fim' para encerrar): ")
+
+while compra != 'fim':
+    compra = input("Digite o nome do produto (ou 'fim' para encerrar): ")
+    compras.append(compra)
+
+
+print(f"sua lista de compras é essa: {compras}")
+
+    
+
+

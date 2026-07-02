2260f17c77ada9551cf8ea04c3c5cf8c25e40345
maria <mariavitoriagoncalves1983@gmail.com>
2026-07-02 09:22:50 -0300

    02/07

--- caça-bugs sqlite3/09_alunos.py
+++ caça-bugs sqlite3/09_alunos.py
@@ -4,7 +4,10 @@ def atualizar_nome_aluno(id_aluno, novo_nome):
     conexao = sqlite3.connect('sistema_escola.db')
     cursor = conexao.cursor()
 
-    cursor.execute("UPDATE alunos SET nome = ?", (novo_nome,))
+    cursor.execute(
+        "UPDATE alunos SET nome = ? WHERE id = ?", (novo_nome, id_aluno))
 
     conexao.commit()
     conexao.close()
+
+# Faltou usar o WHERE no UPDATE, então o sistema mudou o nome de todos os alunos e não só do aluno com o ID informado.
--- caça-bugs sqlite3/10_escolas.py
+++ caça-bugs sqlite3/10_escolas.py
@@ -1,14 +1,13 @@
 import sqlite3
 
 def deletar_escola_antiga():
-    id_escola = int(input(" ID da escola remover: "))
+    id_escola = int(input("ID da escola a remover: "))
     conexao = sqlite3.connect('sistema_escola.db')
-    cursor = conexao.crsor()
-    
-    cursor.execute(f"DELETE FROM escolas WHERE id =? {id_escola}")
+    cursor = conexao.cursor()
 
-    conexao.commit
-    conexao.close
+    cursor.execute("DELETE FROM escolas WHERE id = ?", (id_escola,))
 
-    # faltava um ponto de interrogaçao que mostra o que apagar id_escola
-    # e faltava um f e {} para chamar a variavel id_escola
+    conexao.commit()
+    conexao.close()
+
+    # A variável id_escola foi usada de forma errada no comando DELETE.
